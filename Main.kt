171b895eba270e9a2b19ad45a47923ab661283e1
liviademoraisferreira-dotcom <liviademoraisferreira@gmail.com>
2026-03-18 11:10:57 -0300

    Hora de K. 1.2

---
+++ Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    println("Digite seu nome:")
+    var nome_do_usuario = readln()
+    println("Olá $nome_do_usuario!")
+}
