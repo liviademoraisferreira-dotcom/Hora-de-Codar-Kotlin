4e8e47fbb3eb73117f337ad4b9048b5196b82b9b
liviademoraisferreira-dotcom <liviademoraisferreira@gmail.com>
2026-03-30 16:44:41 -0300

    hora de codar 1.3

---
+++ Hora de codar 1.3.kt
@@ -0,0 +1,8 @@
+fun main(){
+
+    println("Digite seu nome: ")
+    val nome_usuario = readln()
+    println("Agora a sua idade: ")
+    val idade = readln().toInt()
+    println("Olá, $nome_usuario! Você tem $idade anos.")
+}
