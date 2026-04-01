2f8e21dc77d5e4079d29444a7c49b9dfdd651c86
liviademoraisferreira-dotcom <liviademoraisferreira@gmail.com>
2026-04-01 16:32:17 -0300

    Hora de codar 2.8

--- Hora de codar 2.8.kt
+++ Hora de codar 2.8.kt
@@ -1,29 +1,21 @@
 fun main() {
+    val anoAtual = 2026
+    var anoNascimento = 0
+    do {
+        print("Digite seu ano de nascimento: ")
+        anoNascimento = readln().toInt()
+        if (anoNascimento < 1900) {
+            println("Ano inválido! Digite novamente!")
+        }
+    } while (anoNascimento < 1900)
 
-    val numeros = DoubleArray(4)
+    val idade = anoAtual - anoNascimento
 
-    println("Digite 4 números (entre 1 e 9) um de cad vez:")
+    println("Você tem $idade anos")
 
-    for (i in 1..4) {
-        var valor: Double
-        do {
-            print("Número $i: ")
-            valor = readln().toDouble()
-            if (valor <= 0 || valor >= 10) {
-                println("Os numeros devem ser entre 0 e 10.")
-            }
-        } while (valor <= 0 || valor >= 10)
-        numeros[i - 1] = valor
-    }
-
-    val media = numeros.sum() / 4
-
-    println("A média é: ${"%.2f".format(media)}")
-
-    if (media > 5) {
-        println("Você passou no teste!")
-    }
-    else {
-        println("Tente novamente.")
+    if (idade >= 16) {
+        println("Você PODE votar este ano!")
+    } else {
+        println("Você NÃO PODE votar este ano.")
     }
 }
