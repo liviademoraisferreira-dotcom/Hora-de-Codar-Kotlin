b082bcddd89f0c3b05e002f44da6146d1fbc726c
liviademoraisferreira-dotcom <liviademoraisferreira@gmail.com>
2026-04-13 23:13:24 -0300

    Hora de codar 3.11

---
+++ Hora de codar 3.11.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print("Digite um numero: ")
+    val n = readln().toInt()
+
+    for (i in 1..n) {
+        println("\nTabuada do $i:")
+        for (j in 1..10) {
+            println("$i x $j = ${i * j}")
+        }
+    }
+}
